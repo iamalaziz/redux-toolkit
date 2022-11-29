884585f0fe38c6c7cd243644f17853215af8c3cb
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-11-29 17:03:08 +0900

    redux practice

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./features/cart/cartSlice";
+import modalSlice from "./features/modal/modalSlice";
+
+const store = configureStore({
+  reducer: { cart: cartSlice, modal: modalSlice },
+});
+export default store;
